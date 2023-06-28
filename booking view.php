1353355a56947bff92b650fb1b4c8c1e7fed4770
MIHIR <112602753+ravalmihir@users.noreply.github.com>
2023-06-28 15:15:23 +0530

    Add files via upload

---
+++ booking view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking view</title>
+</head>
+<body>
+<h2><a href='currentbooking.php'>[currentbooking page]</a><a href="/bnb/">[Return to main page]</a></h2>
+    <div>
+    <textarea id="text" name="text" rows="15" cols="50">
+        Room name:rock
+
+        Checkin date:2018-09-19
+
+        checkout date:2018-09-21
+        
+        Contact Number:(001)123 1234
+        
+        Extras: with breakfast
+        
+        Room Review:good
+</textarea>
+    </div>
+    
+</body>
+</html>
---
+++ delete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking Deletation</title>
+</head>
+<body>
+<h2><a href='currentbooking.php'>[currentbooking page]</a><a href="/bnb/">[Return to main page]</a></h2>
+<textarea id="text" name="text" rows="8" cols="50">
+        Room name:rock
+
+        Checkin date:2018-09-19
+
+        checkout date:2018-09-19
+        
+    
+</textarea>
+<br>
+<button>Delete</button>
+</body>
+</html>
